e566dc15fbfc0fa8ac5b1fa9540eca9872f85c1c
Mike Sokolenko <mike.sokolenko@gmail.com>
2026-06-23 14:29:46 +0200

    Fix Windows release build

--- desktop_app/src/ffmpeg.rs
+++ desktop_app/src/ffmpeg.rs
@@ -8,6 +8,9 @@ use std::path::Path;
 use crate::paths;
 
 #[cfg(windows)]
+use std::os::windows::process::CommandExt;
+
+#[cfg(windows)]
 const CREATE_NO_WINDOW: u32 = 0x08000000;
 
 #[cfg(windows)]
